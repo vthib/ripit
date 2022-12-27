6b98994c8b7bbd9ba45733b128b5341b71b5d10d
Vincent Thiberville <vthib@pm.me>
2022-12-27 14:17:45 +0100

    feat: update clap to v4

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,8 @@
 use crate::error;
+use clap::{
+    builder::{Arg, Command},
+    ArgAction,
+};
 use serde::Deserialize;
 
 pub struct Branch {
@@ -37,12 +41,85 @@ struct YamlCfg {
 }
 
 pub fn parse_args() -> Result<Options, error::Error> {
-    let yaml = clap::load_yaml!("cli.yml");
-    let matches = clap::App::from_yaml(yaml)
-        .setting(clap::AppSettings::ColoredHelp)
+    let matches = Command::new("ripit")
+        .version("0.9.2")
+        .about("Copy commits between git repositories")
+        // Configuration
+        .arg(
+            Arg::new("config_file")
+                .required(true)
+                .help("Path to configuration file")
+                .long_help(
+                    "A configuration file containing parameters related to the git \
+            repository is required. \
+            To create a new one, duplicate and modify config-template.yml, \
+            which contains descriptions of all possible options.",
+                ),
+        )
+        // Type of action
+        .arg(
+            Arg::new("bootstrap")
+                .action(ArgAction::SetTrue)
+                .long("bootstrap")
+                .help("Bootstrap the local repository")
+                .long_help(
+                    "Before the two repositories can be synchronized, the local \
+            repository must be boostrapped, by creating a single commit \
+            containing the current state of the remote repository. This \
+            is done for each branch to synchronize.",
+                ),
+        )
+        // behavioral features
+        .arg(
+            Arg::new("uproot")
+                .action(ArgAction::SetTrue)
+                .short('u')
+                .long("uproot")
+                .help("Allow commits uprooting")
+                .long_help(
+                    "By default, a commit with an unknown parent cannot be \
+                synchronized. This prevents mistakes and ensures the topology \
+                of the sync'ed repository is preserved. \
+                However, there are some legitimate cases when this situation can \
+                happen, for example when synchronizing a merge commit with \
+                one ancestor dating from prior to the bootstrap. \
+                In that case, we want to cherry-pick the commits brought by \
+                the merge (or in this context, \"uproot\" them). \
+                This behavior can be activated with this flag.",
+                ),
+        )
+        .arg(
+            Arg::new("nofetch")
+                .action(ArgAction::SetTrue)
+                .short('F')
+                .long("no-fetch")
+                .help("Do not fetch private repository")
+                .long_help(
+                    "By default, ripit will fetch the last commits from the private \
+            repository before computing the differences with the local \
+            repository. This behavior can be deactivated with this option, \
+            which can be useful if the fetch requires authentication which \
+            is not handled in ripit.",
+                ),
+        )
+        // common options shared by every action
+        .arg(
+            Arg::new("quiet")
+                .action(ArgAction::SetTrue)
+                .short('q')
+                .long("quiet")
+                .help("Do not print detailed logs of the execution's progress"),
+        )
+        .arg(
+            Arg::new("yes")
+                .action(ArgAction::SetTrue)
+                .short('y')
+                .long("yes")
+                .help("Automatic yes to prompts"),
+        )
         .get_matches();
 
-    let path = matches.value_of("config_file").unwrap();
+    let path = matches.get_one::<String>("config_file").unwrap();
     let file = match std::fs::File::open(path) {
         Ok(f) => f,
         Err(error) => {
@@ -93,10 +170,10 @@ pub fn parse_args() -> Result<Options, error::Error> {
         branches,
         commit_msg_filters,
 
-        bootstrap: matches.is_present("bootstrap"),
-        uproot: matches.is_present("uproot"),
-        verbose: !matches.is_present("quiet"),
-        yes: matches.is_present("yes"),
-        fetch: !matches.is_present("nofetch"),
+        bootstrap: matches.get_flag("bootstrap"),
+        uproot: matches.get_flag("uproot"),
+        verbose: !matches.get_flag("quiet"),
+        yes: matches.get_flag("yes"),
+        fetch: !matches.get_flag("nofetch"),
     })
 }
